0d5f474dc582186bcc5bcf29578842ead3375509
Dino Drmic <iddrmic@gmail.com>
2022-11-02 19:59:27 +0100

    Create a React app and publish it to GitHub Pages

---
+++ weather-app-react/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=03f77b1b3cf019c473cac06b6b542ca4`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+  return (
+    <div className="app">
+      <div className="header">
+        <h1 className="name">WeatherApp</h1>
+      </div>
+      <div className="search">
+        <input type="text"
+               value={location}
+               onChange={event => setLocation(event.target.value)}
+               onKeyPress={searchLocation}
+               placeholder="Enter Location" />
+      </div>
+      <div className="container">
+
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{Math.round(data.main.temp-273.15)} °C</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <img className="icon" src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}/> : null}
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name != undefined && 
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className="bold">{Math.round(data.main.feels_like-273.15)} °C</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity} %</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className="bold">{Math.round(data.wind.speed*1.60934)} km/h</p> : null}
+              <p>Wind</p>
+            </div>
+          </div>
+        }
+      </div>
+    </div>    
+  );
+}
+
+export default App;
